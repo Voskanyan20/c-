cfd77df7cca0150781ee89d2bc505a40076371f8
Voskanyan20 <arpman9900@gmail.com>
2022-11-07 22:32:20 +0400

    max min sum c++

---
+++ numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+        int a = 3;
+        int b = 4;
+
+        std::cout << "max(a,b)==" << std::max(a,b) << '\n';
+	std::cout << "min(a,b)==" << std::min(a,b) << '\n';
+	std::cout << "a + b==" << a + b << std::endl;
+
+        return 0;
+}
+
+
+
